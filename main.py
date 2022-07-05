0eaac903ec8077a8f3bbc9386d5534b384c69812
paps44 <ariyosniche@gmail.com>
2022-07-05 11:45:04 +0200

    first commit

---
+++ main.py
@@ -0,0 +1,7 @@
+
+phrase = "Giraffe Academy"
+print(phrase + " is cool.")
+print(len(phrase +phrase + " is cool." ))
+print(phrase[3])
+print(phrase.index("Acad"))
+print(phrase.replace ("Giraffe", "Leopards"))
