ccdf761eed40818dd987182b4f1bf0531c5f81bb
paps44 <ariyosniche@gmail.com>
2022-07-05 12:00:49 +0200

    Change folder structure

---
+++ strings/Catherine.py
@@ -0,0 +1,14 @@
+
+print('Esther Adebayo')
+print('   /\ ')
+print('  /  \ ')
+print(' /    \ ')
+print('/______\ ')
+print('*' * 10)
+price = 10
+price = 20
+print(price)
+full_name = 'John Smith'
+age = 20
+is_new = True
+
