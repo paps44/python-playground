805c069482a8730a325da1a79da6b0cf56f8ffb4
paps44 <ariyosniche@gmail.com>
2022-07-08 09:29:26 +0200

    Learning Input

--- arrays/Getting Input.py
+++ arrays/Getting Input.py
@@ -1,2 +1,6 @@
 name = input('What is your name? ')
 print('Hi ' + name)
+
+name = input('What is your name? ')
+favourite_colour = input('What is your favourite colour? ')
+print(name  +  ' likes ' + favourite_colour)
