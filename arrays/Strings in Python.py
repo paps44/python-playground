d8807846f00a9a3a63091b4c053cd6fe21239d11
paps44 <ariyosniche@gmail.com>
2022-07-09 08:33:05 +0200

    Strings learning

--- arrays/Strings in Python.py
+++ arrays/Strings in Python.py
@@ -1,2 +1,45 @@
 course = 'Python Course for "Beginners"'
 print(course)
+
+course = ''' 
+Hi Marvin,
+
+The escalation template has been updated.
+Kindly view.
+
+Thank you,
+Billing team
+
+'''
+print(course)
+
+course = 'Python for Beginners'
+print(course[0])
+
+course = 'Python for Beginners'
+print(course[-1])
+
+course = 'Python for Beginners'
+print(course[-2])
+
+course = 'Python for Beginners'
+print(course[0:3])
+
+course = 'Python for Beginners'
+print(course[0:])
+
+course = 'Python for Beginners'
+print(course[1:])
+
+course = 'Python for Beginners'
+print(course[:5])
+
+course = 'Python for Beginners'
+print(course[:])
+
+course = 'Python for Beginners'
+another = course[:]
+print(another)
+
+name = 'Katarina'
+print(name[1:-1])
