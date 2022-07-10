ff13e0f1b52d716f0a28b1c0b1fad44ed11d841c
paps44 <ariyosniche@gmail.com>
2022-07-10 14:38:20 +0200

    learning formatted strings

---
+++ Strings Method.py
@@ -0,0 +1,15 @@
+course ='''
+Doing really cool things with Python Strings
+'''
+print(course)
+
+course = 'Python for Beginners Hands On'
+print(len(course))
+
+course = 'Python for Beginners Hands On'
+print(course.upper())
+print(course.lower())
+print(course)
+
+course = 'Python for Beginners Hands On'
+
