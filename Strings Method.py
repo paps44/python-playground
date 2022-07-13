74d91dd65f452ea3f78d6aff1b11d4100bda0193
paps44 <ariyosniche@gmail.com>
2022-07-13 22:32:49 +0200

    More functions

--- Strings Method.py
+++ Strings Method.py
@@ -11,5 +11,21 @@ print(course.upper())
 print(course.lower())
 print(course)
 
+#course = 'Python for Beginners Hands On'
+#print(course.find('P'))
+
 course = 'Python for Beginners Hands On'
+print(course.find('Beginners'))
+
+course = 'Python for Beginners Hands On'
+print(course.replace('Beginners', 'Absolute Beginners'))
+
+#course = 'Python for Beginners Hands On'
+#print(course.replace('P', 'L'))
+
+course = 'Python for Beginners Hands On'
+print('Beginners' in course)
+
+Course = 'Python for Beginners Hands On'
+print('beginners' in course)
 
