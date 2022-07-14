567310c7e37421017810405d1af670ac62876615
paps44 <ariyosniche@gmail.com>
2022-07-14 21:22:37 +0200

    Operator Precedence

---
+++ Operator Precedence.py
@@ -0,0 +1,19 @@
+#''' (Operator precedence means the order of operations)   '''
+
+x = 10 + 3 * 2
+print(x)
+
+''' Order of operation '''
+#parenthesis
+#exponentiation 2**3
+#multiplication or division
+#addittion or subtraction
+
+x = 10 + 3 * 2 ** 2
+print(x)
+
+x = (10 + 3) * 2 ** 2
+print(x)
+
+a = (2 + 3) * 10 -3
+print(a)
