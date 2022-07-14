096379c7009e2b2283ced8ff291737adf889c64f
paps44 <ariyosniche@gmail.com>
2022-07-14 08:55:23 +0200

    Arithmetic Operations

---
+++ Arithmetic Operations.py
@@ -0,0 +1,23 @@
+course ='''In python programming language we have two types of numbers
+integers(whole numbers) & floats(decimal points)'''
+#print(course)
+
+print(10 + 3) #Addition operator
+print(10 - 3) #Subraction operator
+print(10 * 3) #Multiplication operator
+print(10 / 3) #Division operator floating
+print(10 // 3) #Division operator integer
+print(10 % 3)  #Modulus operator
+print(10 ** 3)
+
+y = 10
+y = y + 3
+print(y)
+
+x = 20
+x -= 3    #Augumented assignment operator
+print(x)
+
+x = 20
+x += 3    #Augumented assignment operator
+print(x)
