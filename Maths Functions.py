4135852f8586e1ef4143b9374f5836db6d6d7d6e
paps44 <ariyosniche@gmail.com>
2022-07-15 04:24:44 +0200

    maths functions

---
+++ Maths Functions.py
@@ -0,0 +1,13 @@
+#'''Useful Functions for Working with Numbers'''
+
+a = 2.9
+print(round(a)) #roundup
+
+q = 10.8
+print(abs(-2.9)) #absolute value
+
+import math
+print(math.ceil(5.9))
+print(math.floor(5.9))
+print(math.factorial(5))
+print(math.copysign(6, 2))
