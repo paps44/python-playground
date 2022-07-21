1bc4b6284369e0e1f5e0f1d136bd5b95a3d10343
paps44 <ariyosniche@gmail.com>
2022-07-21 19:42:21 +0200

    Guessing game

--- The guessing game.py
+++ The guessing game.py
@@ -9,3 +9,35 @@ while guess_count < guess_limit:
         break
 else:
     print('Sorry, you failed!')
+
+#PRACTICE EXERCISE
+#an empty string is one with no characters in, it's an empty quote
+
+command = ""
+started = False
+while True:
+    command = input("> ").lower()
+    if command =="start":
+        if started:
+            print("Car is already started!")
+        else:
+            started = True
+            print("Car started...")
+    elif command == "stop":
+        if not started:
+            print("Car is already stopped!")
+        else:
+            started = False
+            print("Car stopped.")
+    elif command =="help":
+        print("""
+start - to start the car
+stop - to stop the car
+quit - to quit
+        """)
+    elif command =="quit":
+        break
+    else:
+        print("Sorry< I don't understand that")
+
+
