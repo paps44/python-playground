b01ac12cc0e994c688b4d455896ae528e571119f
paps44 <ariyosniche@gmail.com>
2022-07-21 11:19:30 +0200

    While Loop

---
+++ While Loops in Phyton.py
@@ -0,0 +1,15 @@
+#While loops is used to execute a block of codes multiple time
+#they are often used to build interactive programs and games.
+
+i = 1
+while i <= 6:
+    print(i)
+    i = i + 1
+print("Done")
+
+i = 1
+while i <= 6:
+    print("*" * i)
+    i = i + 1
+print("Done")
+
