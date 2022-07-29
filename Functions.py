ca435d9f38c9b4fbc452ad5e57b63df3acbfe6b5
paps44 <ariyosniche@gmail.com>
2022-07-29 10:03:26 +0200

    Functions

---
+++ Functions.py
@@ -0,0 +1,12 @@
+"""As our programs grow we need a better way to organize our code
+we need to break up our code into smaller , more manageable and more
+maintainable chunks which is called Functions. Function is a container for a
+few lines of code that perform a specific task"""
+
+def greet_user():
+    print("Hi there")
+    print("Welcome aboard")
+
+print("Start")
+greet_user()
+print("Finish")
