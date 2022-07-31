7f63ccec813beea7babbb79b2125581141ae24a0
paps44 <ariyosniche@gmail.com>
2022-07-31 09:21:53 +0200

    Return Statement

---
+++ Return Statement.py
@@ -0,0 +1,7 @@
+#Now lets look at how to create functions that return values, this is useful doing
+#calculation with function and we want to return result to whoever is using our F!
+
+def square(number):
+    return number * number
+
+print(square(3))
