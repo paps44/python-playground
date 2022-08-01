8710a8d2976f8e8720cace0aad1d2f3c6b63cf59
paps44 <ariyosniche@gmail.com>
2022-08-01 14:17:26 +0200

    Comments

---
+++ Comments.py
@@ -0,0 +1,17 @@
+# We use comments to add note or comment to our program
+
+# we can use comment to explain something about our code, we can use it as reminders
+# to fix things or clear things up or we can use it to communicate things with other
+# developers why we have written a code in a certain way. These are good use cases for using comment
+# One thing we need to avoid when writing a comment is explaining what a code does.
+# if certain assumptions has been made we can use comment to explain to remind yourself
+# or other developers
+
+# print sky is blue (bad comment) because its telling me the obvious
+print("Sky is blue")
+
+# calculate and return the square of a number(noisy, repetitive & distracting)
+def square(number):
+    return number * number
+
+# So it's better to use comments to explain why's and how not what
